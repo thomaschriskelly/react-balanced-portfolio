1aadd24907078a3d496337e39ca7ad9badfd642f
Tom Kelly <thomaschriskelly@gmail.com>
2021-01-01 20:54:03 -0500

    copy changes to simplify

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [canadianStocks, setCanadianStocks] = useState('');
-  const [canadianBonds, setCanadianBonds] = useState('');
-  const [usaStocks, setUsaStocks] = useState('');
-  const [intlStocks, setIntlStocks] = useState('');
+  const [canadianIndex, setCanadianIndex] = useState('');
+  const [usaIndex, setUsaIndex] = useState('');
+  const [canadianBondIndex, setCanadianBondIndex] = useState('');
+  const [internationalIndex, setInternationalIndex] = useState('');
   const [amountToPurchase, setAmountToPurchase] = useState('');
 
-  const canadianStocksNum = parseFloat(canadianStocks) || 0;
-  const canadianBondsNum = parseFloat(canadianBonds) || 0;
-  const usaStocksNum = parseFloat(usaStocks) || 0;
-  const intlStocksNum = parseFloat(intlStocks) || 0;
-  const currentTotal = canadianStocksNum + canadianBondsNum + usaStocksNum + intlStocksNum;
-  const amountToPurchaseNum = parseFloat(amountToPurchase)
+  const canadianIndexNum = parseFloat(canadianIndex) || 0;
+  const canadianBondIndexNum = parseFloat(canadianBondIndex) || 0;
+  const usaIndexNum = parseFloat(usaIndex) || 0;
+  const internationalIndexNum = parseFloat(internationalIndex) || 0;
+  const currentTotal = canadianIndexNum + canadianBondIndexNum + usaIndexNum + internationalIndexNum;
+  const amountToPurchaseNum = parseFloat(amountToPurchase) || 0;
   const newTotal = currentTotal + amountToPurchaseNum;
   const desiredPer = Math.floor(newTotal / 4);
 
@@ -27,42 +27,42 @@ function App() {
         <h3>Current market values</h3>
         <div>
           <label>
-            Canadian Stocks:
-            <input type="text" value={canadianStocks} onChange={(e) => setCanadianStocks(e.target.value)} />
+            Canadian Index (TDB900):
+            <input type="text" value={canadianIndex} onChange={(e) => setCanadianIndex(e.target.value)} />
           </label>
         </div>
         <div>
           <label>
-            Canadian Bonds:
-            <input type="text" value={canadianBonds} onChange={(e) => setCanadianBonds(e.target.value)} />
+            USA Stocks (TDB902):
+            <input type="text" value={usaIndex} onChange={(e) => setUsaIndex(e.target.value)} />
           </label>
         </div>
         <div>
           <label>
-            USA Stocks:
-            <input type="text" value={usaStocks} onChange={(e) => setUsaStocks(e.target.value)} />
+            Canadian Bond Index (TDB909):
+            <input type="text" value={canadianBondIndex} onChange={(e) => setCanadianBondIndex(e.target.value)} />
           </label>
         </div>
         <div>
           <label>
-            International Stocks:
-            <input type="text" value={intlStocks} onChange={(e) => setIntlStocks(e.target.value)} />
+            International Index (TDB911):
+            <input type="text" value={internationalIndex} onChange={(e) => setInternationalIndex(e.target.value)} />
           </label>
         </div>
 
-        <h3>Amount to purchase</h3>
+        <h3>Amount to buy</h3>
         <label>
           <input type="text" value={amountToPurchase} onChange={(e) => setAmountToPurchase(e.target.value)} />
         </label>
 
         <h3>Therefore...</h3>
         <p>You currently have ${currentTotal.toLocaleString()} in your portfolio</p>
-        <p>With deposit, you will have ${newTotal.toLocaleString()} in your portfolio</p>
+        <p>With buys, you will have ${newTotal.toLocaleString()} in your portfolio</p>
         <p>You want ${desiredPer.toLocaleString()} per fund</p>
-        <p>Deposit ${Math.floor(desiredPer - canadianStocksNum).toLocaleString()} into Canadian Stocks</p>
-        <p>Deposit ${Math.floor(desiredPer - canadianBondsNum).toLocaleString()} into Canadian Bonds</p>
-        <p>Deposit ${Math.floor(desiredPer - usaStocksNum).toLocaleString()} into USA Stocks</p>
-        <p>Deposit ${Math.floor(desiredPer - intlStocksNum).toLocaleString()} into International Stocks</p>
+        <p>Buy ${Math.floor(desiredPer - canadianIndexNum).toLocaleString()} Canadian index (TDB900)</p>
+        <p>Buy ${Math.floor(desiredPer - usaIndexNum).toLocaleString()} USA index (TDB902)</p>
+        <p>Buy ${Math.floor(desiredPer - canadianBondIndexNum).toLocaleString()} Canadian bond index (TDB909)</p>
+        <p>Buy ${Math.floor(desiredPer - internationalIndexNum).toLocaleString()} International index (TDB911)</p>
       </header>
     </div>
   );
