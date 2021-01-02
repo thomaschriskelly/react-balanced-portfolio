1c43558519cb234b7da58c40adb28bc10a01b163
Tom Kelly <thomaschriskelly@gmail.com>
2021-01-01 20:17:46 -0500

    portfolio balancer does math based on https://github.com/thomaschriskelly/balanced-portfolio/blob/master/balance.py

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,21 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [stocks, setStocks] = useState('');
+  const [canadianStocks, setCanadianStocks] = useState('');
+  const [canadianBonds, setCanadianBonds] = useState('');
+  const [usaStocks, setUsaStocks] = useState('');
+  const [intlStocks, setIntlStocks] = useState('');
+  const [amountToPurchase, setAmountToPurchase] = useState('');
+
+  const canadianStocksNum = parseFloat(canadianStocks) || 0;
+  const canadianBondsNum = parseFloat(canadianBonds) || 0;
+  const usaStocksNum = parseFloat(usaStocks) || 0;
+  const intlStocksNum = parseFloat(intlStocks) || 0;
+  const currentTotal = canadianStocksNum + canadianBondsNum + usaStocksNum + intlStocksNum;
+  const amountToPurchaseNum = parseFloat(amountToPurchase)
+  const newTotal = currentTotal + amountToPurchaseNum;
+  const desiredPer = newTotal / 4;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -14,39 +28,41 @@ function App() {
         <div>
           <label>
             Canadian Stocks:
-            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+            <input type="text" value={canadianStocks} onChange={(e) => setCanadianStocks(e.target.value)} />
           </label>
         </div>
         <div>
           <label>
             Canadian Bonds:
-            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+            <input type="text" value={canadianBonds} onChange={(e) => setCanadianBonds(e.target.value)} />
           </label>
         </div>
         <div>
           <label>
             USA Stocks:
-            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+            <input type="text" value={usaStocks} onChange={(e) => setUsaStocks(e.target.value)} />
           </label>
         </div>
         <div>
           <label>
             Intl Stocks:
-            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+            <input type="text" value={intlStocks} onChange={(e) => setIntlStocks(e.target.value)} />
           </label>
         </div>
 
         <h3>Amount to purchase</h3>
         <label>
-          <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+          <input type="text" value={amountToPurchase} onChange={(e) => setAmountToPurchase(e.target.value)} />
         </label>
 
         <h3>Therefore...</h3>
-        <p>You want $x per fund</p>
-        <p>Deposit $x into Canadian Stocks</p>
-        <p>Deposit $x into Canadian Bonds</p>
-        <p>Deposit $x into USA Stocks</p>
-        <p>Deposit $x into Intl Stocks</p>
+        <p>You currently have ${currentTotal} in your portfolio</p>
+        <p>With the amount you are depositing, you will have ${newTotal} in your portfolio</p>
+        <p>You want ${desiredPer} per fund</p>
+        <p>Deposit ${desiredPer - canadianStocksNum} into Canadian Stocks</p>
+        <p>Deposit ${desiredPer - canadianBondsNum} into Canadian Bonds</p>
+        <p>Deposit ${desiredPer - usaStocksNum} into USA Stocks</p>
+        <p>Deposit ${desiredPer - intlStocksNum} into Intl Stocks</p>
       </header>
     </div>
   );
