094c1a5a460d9fb7d706b526221f160ad9e9df14
Tom Kelly <thomaschriskelly@gmail.com>
2021-01-01 20:05:35 -0500

    Barebones UI for balancing

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [stocks, setStocks] = useState('');
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>
+          Portfolio Balancer
+        </h1>
+
+        <h3>Current Market Values</h3>
+        <div>
+          <label>
+            Canadian Stocks:
+            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Canadian Bonds:
+            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            USA Stocks:
+            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+          </label>
+        </div>
+        <div>
+          <label>
+            Intl Stocks:
+            <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+          </label>
+        </div>
+
+        <h3>Amount to purchase</h3>
+        <label>
+          <input type="text" value={stocks} onChange={(e) => setStocks(e.target.value)} />
+        </label>
+
+        <h3>Therefore...</h3>
+        <p>You want $x per fund</p>
+        <p>Deposit $x into Canadian Stocks</p>
+        <p>Deposit $x into Canadian Bonds</p>
+        <p>Deposit $x into USA Stocks</p>
+        <p>Deposit $x into Intl Stocks</p>
       </header>
     </div>
   );
