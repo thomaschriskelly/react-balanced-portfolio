28cd3b8df4e32a4131c7d4af5419e5b6565965b8
Tom Kelly <thomaschriskelly@gmail.com>
2021-01-02 12:29:42 -0500

    added desired breakdown

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,15 @@ interface Fund {
   name: string;
   code: string;
   amount: string;
+  percent: string;
 }
 
 function App() {
   const [funds, setFunds] = useState<Fund[]>([
-    { name: 'Canadian Index', code: 'TDB900', amount: '' },
-    { name: 'USA Index', code: 'TDB902', amount: '' },
-    { name: 'Canadian Bonds', code: 'TDB909', amount: '' },
-    { name: 'International Index', code: 'TDB911', amount: '' },
+    { name: 'Canadian Index', code: 'TDB900', amount: '', percent: '25' },
+    { name: 'USA Index', code: 'TDB902', amount: '', percent: '25' },
+    { name: 'Canadian Bonds', code: 'TDB909', amount: '', percent: '25' },
+    { name: 'International Index', code: 'TDB911', amount: '', percent: '25' },
   ]);
   const [amountToPurchase, setAmountToPurchase] = useState('');
 
@@ -19,9 +20,15 @@ function App() {
     (previous, current) => previous + parseFloat(current.amount) || 0,
     0,
   );
+  const totalPercent = funds.reduce(
+    (previous, current) => previous + parseInt(current.percent) || 0,
+    0,
+  );
   const amountToPurchaseNum = parseFloat(amountToPurchase) || 0;
   const newTotal = currentTotal + amountToPurchaseNum;
   const desiredPer = Math.floor(newTotal / funds.length);
+
+  const validTotalPercent = totalPercent === 100;
 
   return (
     <div className="App" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
@@ -31,7 +38,7 @@ function App() {
       {funds.map((fund, index) => (
         <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
           <label>
-            {fund.name} ({fund.code}):
+            $ {fund.name} ({fund.code}):
           </label>
           <input
             type="text"
@@ -63,14 +70,40 @@ function App() {
         <span>${newTotal.toLocaleString()}</span>
       </div>
 
-      <h3>Therefore...</h3>
-      <p>You want ${desiredPer.toLocaleString()} per fund</p>
-      {funds.map((fund) => (
-        <p>
-          Buy ${Math.floor(desiredPer - parseFloat(fund.amount) || 0).toLocaleString()} of{' '}
-          {fund.name} ({fund.code})
-        </p>
+      <h3>Desired breakdown</h3>
+      {funds.map((fund, index) => (
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
+          <label>
+            % {fund.name} ({fund.code}):
+          </label>
+          <input
+            type="text"
+            value={fund.percent}
+            onChange={(e) => {
+              const newFunds = [...funds];
+              newFunds[index].percent = e.target.value;
+              setFunds(newFunds);
+            }}
+          />
+        </div>
       ))}
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
+        <label>Total:</label>
+        <span style={{ color: validTotalPercent ? undefined : 'red' }}>{totalPercent}%</span>
+      </div>
+
+      {validTotalPercent ? (
+        <>
+          <h3>Therefore...</h3>
+          <p>You want ${desiredPer.toLocaleString()} per fund</p>
+          {funds.map((fund) => (
+            <p>
+              Buy ${Math.floor(desiredPer - parseFloat(fund.amount) || 0).toLocaleString()} of{' '}
+              {fund.name} ({fund.code})
+            </p>
+          ))}
+        </>
+      ) : null}
     </div>
   );
 }
