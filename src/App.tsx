50cf5eccd4ab28adc793f0949319c3b1105ffb6d
Tom Kelly <thomaschriskelly@gmail.com>
2021-01-15 22:55:47 -0500

    Naive sale minimizer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
 import React, { useState } from 'react';
 
-interface Fund {
+interface IFund {
   name: string;
   code: string;
   currentAmount: string;
   targetPercent: string;
 }
 
+interface IFundWithTransaction extends IFund {
+  target: number;
+  difference: number;
+}
+
 function App() {
-  const [funds, setFunds] = useState<Fund[]>([
+  const [funds, setFunds] = useState<IFund[]>([
     { name: 'Canadian Index', code: 'TDB900', currentAmount: '', targetPercent: '25' },
     { name: 'USA Index', code: 'TDB902', currentAmount: '', targetPercent: '25' },
     { name: 'Canadian Bonds', code: 'TDB909', currentAmount: '', targetPercent: '25' },
@@ -21,7 +26,7 @@ function App() {
     0,
   );
   const totalPercent = funds.reduce(
-    (previous, current) => previous + (parseInt(current.targetPercent) || 0),
+    (previous, current) => previous + (parseFloat(current.targetPercent) || 0),
     0,
   );
   const validTotalPercent = totalPercent === 100;
@@ -29,13 +34,36 @@ function App() {
   const amountToPurchaseNum = parseFloat(amountToPurchase) || 0;
   const newTotal = currentTotal + amountToPurchaseNum;
 
+  const massagedFunds: IFundWithTransaction[] = funds.map((fund) => {
+    const { currentAmount, targetPercent } = fund;
+    const currentAmountFloat = parseFloat(currentAmount) || 0;
+    const percentMultiplier = (parseInt(targetPercent) || 0) / 100;
+    const target = percentMultiplier * newTotal;
+    const difference = target - currentAmountFloat;
+    return { ...fund, target, difference };
+  });
+
+  const fundsToSell = massagedFunds.filter((fund) => fund.difference < 0);
+  const totalSell = fundsToSell.reduce((prev, curr) => prev + curr.difference, 0);
+  const fundsToBuy = massagedFunds.filter((fund) => fund.difference >= 0);
+  const newDenominator = fundsToBuy.reduce(
+    (prev, curr) => prev + (parseInt(curr.targetPercent) || 0),
+    0,
+  );
+  const fundsToBuyWithoutSelling: IFundWithTransaction[] = fundsToBuy.map((fund) => {
+    const newPercentMultiplier = (parseInt(fund.targetPercent) || 0) / newDenominator;
+    const difference = fund.difference + totalSell * newPercentMultiplier;
+    const target = (parseFloat(fund.currentAmount) || 0) + difference;
+    return { ...fund, target, difference };
+  });
+
   return (
     <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
       <h1>Portfolio Balancer</h1>
 
       <h3>Current market values</h3>
       {funds.map((fund, index) => (
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
           <label>
             $ {fund.name} ({fund.code}):
           </label>
@@ -50,13 +78,13 @@ function App() {
           />
         </div>
       ))}
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
         <label>Total:</label>
         <span>${currentTotal.toLocaleString()}</span>
       </div>
 
       <h3>Amount to buy</h3>
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
         <label>Amount in CAD</label>
         <input
           type="number"
@@ -64,14 +92,14 @@ function App() {
           onChange={(e) => setAmountToPurchase(e.target.value)}
         />
       </div>
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
         <label>New total:</label>
         <span>${newTotal.toLocaleString()}</span>
       </div>
 
       <h3>Desired breakdown</h3>
       {funds.map((fund, index) => (
-        <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
           <label>
             % {fund.name} ({fund.code}):
           </label>
@@ -86,7 +114,7 @@ function App() {
           />
         </div>
       ))}
-      <div style={{ display: 'flex', justifyContent: 'space-between', width: '300px' }}>
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '400px' }}>
         <label>Total:</label>
         <span style={{ color: validTotalPercent ? undefined : 'red' }}>{totalPercent}%</span>
       </div>
@@ -94,45 +122,44 @@ function App() {
       {validTotalPercent ? (
         <>
           <h3>Therefore...</h3>
-          <ul>
-            {funds.map((fund) => {
-              const { currentAmount, targetPercent } = fund;
-              const currentAmountFloat = parseFloat(currentAmount) || 0;
-              const percentMultiplier = (parseInt(targetPercent) || 0) / 100;
-              const target = percentMultiplier * newTotal;
-              const difference = target - currentAmountFloat;
-              return (
-                <li>
-                  {difference >= 0 ? (
-                    <>
-                      Buy $
-                      {difference.toLocaleString(undefined, {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                      })}
-                    </>
-                  ) : (
-                    <>
-                      Sell $
-                      {(-difference).toLocaleString(undefined, {
-                        minimumFractionDigits: 2,
-                        maximumFractionDigits: 2,
-                      })}
-                    </>
-                  )}{' '}
-                  of {fund.name} ({fund.code}) to hit $
-                  {target.toLocaleString(undefined, {
-                    minimumFractionDigits: 2,
-                    maximumFractionDigits: 2,
-                  })}
-                </li>
-              );
-            })}
-          </ul>
+          <ul>{massagedFunds.map(Fund)}</ul>
+          {fundsToSell.length ? (
+            <>
+              <h3>To minimize selling...</h3>
+              <ul>{fundsToBuyWithoutSelling.map(Fund)}</ul>
+            </>
+          ) : null}
         </>
       ) : null}
     </div>
   );
 }
+
+const Fund: React.FC<IFundWithTransaction> = ({ name, code, target, difference }) => (
+  <li>
+    {difference >= 0 ? (
+      <>
+        Buy $
+        {difference.toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
+      </>
+    ) : (
+      <>
+        Sell $
+        {(-difference).toLocaleString(undefined, {
+          minimumFractionDigits: 2,
+          maximumFractionDigits: 2,
+        })}
+      </>
+    )}{' '}
+    of {name} ({code}) to hit $
+    {target.toLocaleString(undefined, {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })}
+  </li>
+);
 
 export default App;
