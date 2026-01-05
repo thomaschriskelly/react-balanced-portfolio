65f95fe2eb5299ac86e0cff8054d36886dc18947
Tom Kelly <tom@cambio.ai>
2026-01-05 13:16:15 -0500

    github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/react-balanced-portfolio',
+  assetPrefix: '/react-balanced-portfolio',
 };
 
 export default nextConfig;
